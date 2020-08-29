641df7d99102f4f86888622110957914e1ebd21a
Glitch (express-lesson-03-1) <none>
2020-08-29 07:10:17 +0000

    🎸🚕 Updated with Glitch - Lesson 3-1

--- server.js
+++ server.js
@@ -3,23 +3,38 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+var todos= ["Đi chợ", "Nấu cơm", "Rửa bát", "Học code tại CodersX"];
 // https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.get('/todos', (req, res) => {
   res.render('todos/index', {
-    todos: ['Đi chợ','Nấu cơm', 'Rửa bát', 'Học code tại CodersX']
+    todos: todos
+  });
+});
+
+app.get('/todos/search', (req, res) => {
+  var q = req.query.q;
+  console.log(q);
+  
+  var matchedItems = todos.filter(function(item){
+    return item.toLowerCase().indexOf(q) !== -1;
+  });
+  
+  res.render('todos/index', {
+    todos: matchedItems
   });
 });
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
-  console.log('Server listening on port ' + process.env.PORT);
+  console.log("Server listening on port " + process.env.PORT);
 });
